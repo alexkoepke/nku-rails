c41e5601b01f7dd3aa14b7c44d70d10ff9fca4e5
Cameron Dixon <cam.dixon@gmail.com>
2014-02-11 22:23:07 +0000

    add a session controller

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ActiveRecord::Base
   def avatar
     Avatar.from_student(self)
   end
+  
+  has_secure_password
+  attr_accessible :email, :password, :password_confirmation, :name, :nickname, :image_url
+  validates_uniqueness_of :email
 end
