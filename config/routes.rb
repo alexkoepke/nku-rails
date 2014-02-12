39d366aec0a4dcf950baa11c30eb099037c645ad
Cameron Dixon <cam.dixon@gmail.com>
2014-02-12 00:42:35 +0000

    add routes and login info

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 NkuRails::Application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   
   resources :posts do
