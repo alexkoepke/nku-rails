f520d3ced002ddd925b4467c0c75cc00fd4ea0ee
Cameron Dixon <cam.dixon@gmail.com>
2014-02-15 19:07:21 +0000

    add attendances and form

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,14 @@
+class AttendancesController < ApplicationController
+  
+  def index
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    @student = Student.find_by_email(params[:email])
+    if @student && @student.authenticate(params[:password])
+      session[:student_id] = @student.id
+      redirect_to @student, notice: "Logged in as #{@student.name}"
+    else
+      flash.now.alert = "Email or password is invalid"
+      render "new"
+    end
+  end
+  
+  def destroy
+    session[:student_id] = nil
+    redirect_to root_url, notice: "Logged out!"
+  end
+  
+end
