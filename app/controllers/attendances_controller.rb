5f65be8be2389aa9f73c42687bf21df2173839c4
Alexander <alex.koepke@gmail.com>
2014-02-15 21:22:43 +0000

    fix controller logged in attendance

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,24 +4,15 @@ class AttendancesController < ApplicationController
     @attendance = Attendance.new
   end
   
-  def new
-    #we created a method @current_student last assignment to help detect if they are logged in
-    #if they are logged in, have them attend
-    #@attendance = Attendance.new
-    #@attendance.seat = params[:attendance][:seat]
-    #if @student = current_student
-    #  #used this line from students controller and reapplied it
-    #  @attendance = Attendance.new      
-    #else #redirect to home
-    #  redirect_to students_home
-    #end
+  def news
   end
   
   def create
-    if @student && @student == curent_student
-    
+    @student = current_student
+    if @student
+      redirect_to attendances_path, notice: 'Here!'
     else 
-      redirect_to students_path, notice: 'You have to log in'
+      redirect_to students_path, notice: 'You have to login'
     end
   end
   
