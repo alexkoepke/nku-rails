d5fae951c85e3ba941dff14909eb8194c54aa124
Alexander <alex.koepke@gmail.com>
2014-02-21 19:01:32 +0000

    add student form, add student data base

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,19 @@
 class StudentsController < ApplicationController
   def index
   end
+  
+  def new
+  end
+  
+  def create
+    @student = Student.new(student_params)
+    
+    @student.save
+    redirect_to @student
+  end
+  
+  private
+    def student_params
+      params.require(:student).permit(:name, :nickname, :email, :image_url)
+    end
 end
