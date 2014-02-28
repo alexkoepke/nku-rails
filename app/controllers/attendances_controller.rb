107b9cc891553870962b7ea4e1e0b6a46cd00cbe
Alexander <alex.koepke@gmail.com>
2014-02-28 20:36:27 +0000

    broken 1-4

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,14 +1,10 @@
 class AttendancesController < ApplicationController
-  
-  def index
-    @attendances = Attendance.all
-  end
-  
   def new
     @attendance = Attendance.new
   end
   
   def create
+    
     if @student = current_student
       @attendance = Attendance.new
       @attendance.seat = params[:attendance][:seat]
@@ -23,6 +19,10 @@ class AttendancesController < ApplicationController
     else
       redirect_to students_path, notice: "You need to login to be marked in attendance"
     end
+  end
+  
+  def index
+    @attendances = Attendance.all
   end
   
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,7 +1,11 @@
 class Attendance < ActiveRecord::Base
   belongs_to :student
   
-  validates_numericality_of :seat, :only_integer => true
-  validates_inclusion_of :seat, :in => 1..4, message: ":   Please enter a seat 1 - 4 only"
+  validates :seat, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
+  #validates_numericality_of :seat, :only_integer => true, message: ":   Please enter a seat 1 - 4 only"
+  #validates_numericality_of :seat, greater_than_or_equal_to: 1, message: ":   Please enter a seat 1 - 4 only"
+  #validates_numericality_of :seat, less_than_or_equal_to: 4, message: ":   Please enter a seat 1 - 4 only"
+  
+  
   
 end
